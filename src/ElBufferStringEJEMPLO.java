e5ab846bda1ee7ba3f311a8e3f3edb46479991f5
Christian Omar <chris_ortiz195@hotmail.com>
2024-03-06 20:06:51 -0600

    NewLittleProjects

---
+++ src/ElBufferStringEJEMPLO.java
@@ -0,0 +1,11 @@
+public class ElBufferStringEJEMPLO {
+    public static void main(String[] args) {
+        StringBuffer prueba = new StringBuffer();
+
+        prueba.append(32);
+        prueba.append(43);
+        System.out.println(prueba);
+
+
+    }
+}
---
+++ src/stringbuilder2.java
@@ -0,0 +1,15 @@
+public class stringbuilder2 {
+    //make mane examples of stringbuilder
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Hola");
+        sb.append(" ");
+        sb.append("que");
+        sb.append(" ");
+        sb.append("tal");
+
+    sb.reverse();
+    System.out.println(sb);
+    }
+}
